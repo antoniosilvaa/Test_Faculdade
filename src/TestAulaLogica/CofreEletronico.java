f6fe5995e7fbc5ea3efdd860ed3e8af9a6183fe0
antonio <antg3102@gmail.com>
2025-04-10 11:21:09 -0300

    Projetos de Logica de programação da Faculdade

--- src/TestAulaLogica/CofreEletronico.java
+++ src/TestAulaLogica/CofreEletronico.java
@@ -1,18 +1,20 @@
 package TestAulaLogica;
 
+import javax.swing.*;
+
 public class CofreEletronico {
     public static void main(String[] args) {
-        int senha1 = 10;
-        int senha2 = 35;
-        int senhaCoringa = 40;
+        String senha1 = JOptionPane.showInputDialog ("Digite a senha 1");
+        String senha2 = JOptionPane.showInputDialog ("Digite a senha 2");
+        String senhaCoringa = JOptionPane.showInputDialog("Digite a senha Master");
 
-        if (senha1 == 20 && senha2 == 30) {
-            System.out.println("acesso concedido");
+        if (senha1 != null && senha2 != null && senha1.equals("12") && senha2.equals("13")) {
+            JOptionPane.showMessageDialog(null,"Voce desbloqueou o cofre", " Sucesso", JOptionPane.INFORMATION_MESSAGE);
 
-        } else if (senhaCoringa == 78) {
-            System.out.println("Acesso permitido pela senha Coringa");
+        } else if (senhaCoringa != null && senhaCoringa.equals("23")) {
+            JOptionPane.showMessageDialog(null, "Voce desbloqueou o cofre com a senha Master", " Sucesso", JOptionPane.INFORMATION_MESSAGE);
         } else {
-            System.out.println("Acesso negado");
+            JOptionPane.showMessageDialog(null, "Senha invalida", "ERROR404", JOptionPane.ERROR_MESSAGE);
         }
     }
 }
