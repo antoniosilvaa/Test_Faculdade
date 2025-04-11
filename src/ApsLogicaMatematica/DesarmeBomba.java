576836572e97c47f2ea7f95d8f2075e0f38438d9
antonio <antg3102@gmail.com>
2025-04-11 12:08:00 -0300

    APS Logica matematica FACULDADE

--- src/ApsLogicaMatematica/DesarmeBomba.java
+++ src/ApsLogicaMatematica/DesarmeBomba.java
@@ -5,10 +5,16 @@ import javax.swing.*;
 
 public class DesarmeBomba {
     public static void main(String[] args) {
-        String codigo1 = JOptionPane.showInputDialog("Digite o primeiro codigo:");
-        String codigo2 = JOptionPane.showInputDialog("Digite o segundo codigo:");
 
-        if (codigo1 != null && codigo2 != null && codigo1.equals("12") && codigo2.equals("13")){
+        String codigo1 = JOptionPane.showInputDialog(null, "Digite o primeiro codigo:", "DESARME A BOMBA!!!", JOptionPane.WARNING_MESSAGE);
+        String codigo2 = JOptionPane.showInputDialog(null,"Digite o segundo codigo:", "DESARME A BOMBA!!!", JOptionPane.WARNING_MESSAGE);
+
+        if (codigo1 == null || codigo2 == null || codigo1.isEmpty() || codigo2.isEmpty()){
+            JOptionPane.showMessageDialog(null, "INSIRA ALGUM VALOR NUMERICO PARA DESARMAR A BOMBA!!", "ERROR", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        if (codigo1.equals("11") && codigo2.equals("13")){
             JOptionPane.showMessageDialog(null, "\uD83D\uDCA5 A BOMBA EXPLODIU \uD83D\uDCA5", "ALERTA!!", JOptionPane.ERROR_MESSAGE);
         }else {
             JOptionPane.showMessageDialog(null,"✅ Você desarmou a bomba!", "SUCESSO", JOptionPane.INFORMATION_MESSAGE);
--- src/TestAulaLogica/CofreEletronico.java
+++ src/TestAulaLogica/CofreEletronico.java
@@ -4,14 +4,19 @@ import javax.swing.*;
 
 public class CofreEletronico {
     public static void main(String[] args) {
-        String senha1 = JOptionPane.showInputDialog ("Digite a senha 1");
-        String senha2 = JOptionPane.showInputDialog ("Digite a senha 2");
-        String senhaCoringa = JOptionPane.showInputDialog("Digite a senha Master");
+        String senha1 = JOptionPane.showInputDialog (null,"Digite a senha 1", "Insira a Senha:", JOptionPane.PLAIN_MESSAGE);
+        String senha2 = JOptionPane.showInputDialog (null,"Digite a senha 2", "Insira a Senha:", JOptionPane.PLAIN_MESSAGE);
+        String senhaCoringa = JOptionPane.showInputDialog(null,"Digite a senha Master", "Insira a Senha:", JOptionPane.PLAIN_MESSAGE);
 
-        if (senha1 != null && senha2 != null && senha1.equals("12") && senha2.equals("13")) {
+        if (senha1 == null || senha2 == null || senhaCoringa == null || senha1.isEmpty() || senha2.isEmpty() || senhaCoringa.isEmpty()){
+            JOptionPane.showMessageDialog(null, "INSIRA ALGUMA SENHA", "ERROR", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        if (senha1.equals("12") && senha2.equals("13")) {
             JOptionPane.showMessageDialog(null,"Voce desbloqueou o cofre", " Sucesso", JOptionPane.INFORMATION_MESSAGE);
 
-        } else if (senhaCoringa != null && senhaCoringa.equals("23")) {
+        } else if (senhaCoringa.equals("23")) {
             JOptionPane.showMessageDialog(null, "Voce desbloqueou o cofre com a senha Master", " Sucesso", JOptionPane.INFORMATION_MESSAGE);
         } else {
             JOptionPane.showMessageDialog(null, "Senha invalida", "ERROR404", JOptionPane.ERROR_MESSAGE);
